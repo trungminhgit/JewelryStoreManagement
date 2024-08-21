54620d52ceb5e286f6e609df96b98eafbc0c12d2
Trung Minh <dangtrungminh456@gmail.com>
2024-08-21 09:01:38 +0700

    create backend folder

---
+++ Backend/jewelrystore/src/main/java/com/dtm/jewelrystore/JewelrystoreApplication.java
@@ -0,0 +1,13 @@
+package com.dtm.jewelrystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelrystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelrystoreApplication.class, args);
+	}
+
+}
---
+++ Backend/jewelrystore/src/test/java/com/dtm/jewelrystore/JewelrystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtm.jewelrystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelrystoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
