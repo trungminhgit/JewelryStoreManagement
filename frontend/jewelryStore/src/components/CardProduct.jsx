9d24725a6ebb1bde195015ece1bcd7b8bdf3c4c2
minhnguyen957 <148942661+minhnguyen957@users.noreply.github.com>
2024-09-08 17:28:39 +0700

    feature:home page by client

--- frontend/jewelryStore/src/components/CardProduct.jsx
+++ frontend/jewelryStore/src/components/CardProduct.jsx
@@ -1,16 +1,28 @@
 import {useContext} from "react";
 import {cartContext} from "../helper/Context.js";
+import {toast, ToastContainer} from "react-toastify";
 
 export default function CardProduct({id=null,name, subname, description, imageUrl}) {
     const {cart ,cartDispatch} = useContext(cartContext);
     console.log(cart);
     const addToCart = (id) => {
-        cartDispatch({
-            type:"add",
-            payload:{
-                id:id
-            }
-        })
+
+        toast.success("Add successfully", {onClose:()=>{
+                cartDispatch({
+                    type:"add",
+                    payload:{
+                        id:id
+                    }
+                })
+            }, onClick:()=>{
+                cartDispatch({
+                    type:"add",
+                    payload:{
+                        id:id
+                    }
+                })
+            }})
+
     }
     const subtractToCart = (id) => {
         cartDispatch({
@@ -23,9 +35,10 @@ export default function CardProduct({id=null,name, subname, description, imageUr
 
     return (
         <>
+            <ToastContainer position='top-right'/>
             <div className="px-8 py-6 bg-purple-400 text-white rounded-lg max-w-[400px]">
                 <div>
-                    <img src={imageUrl} alt="image" className="object-cover" />
+                    <img src={imageUrl} alt="image" className="object-cover aspect-square" />
                 </div>
                 <div className="my-4">
                     <h3 className="text-4xl">{name}</h3>
@@ -34,8 +47,7 @@ export default function CardProduct({id=null,name, subname, description, imageUr
                 <p className="text-wrap break-all block">
                     {description}
                 </p>
-                <button onClick={()=>addToCart(id)} className="bg-yellow-400 px-4 py-2 text-white rounded-md mt-8">+</button>
-                <button onClick={()=>subtractToCart(id)} className="bg-yellow-400 px-4 py-2 text-white rounded-md mt-8 ml-3">-</button>
+                <button onClick={()=>addToCart(id)} className="bg-yellow-400 px-4 py-2 text-white rounded-md mt-8">Add to cart</button>
             </div>
         </>
     )
--- frontend/jewelryStore/src/helper/Reducer.js
+++ frontend/jewelryStore/src/helper/Reducer.js
@@ -45,10 +45,10 @@ export const cartInitState = {
 export const cartReducer = (state = cartInitState, action) => {
     switch (action.type) {
         case 'add':
+        {
             const exitsItem = state.cart.find(item => item.id === action.payload.id)
             if (exitsItem) {
                 return {
-                    ...state,
                     cart: state.cart.map(item => item.id === action.payload.id ? {
                         ...item,
                         quantity: item.quantity + 1
@@ -56,10 +56,10 @@ export const cartReducer = (state = cartInitState, action) => {
                 }
             } else {
                 return {
-                    ...state,
                     cart: [...state.cart, {...action.payload, quantity: 1}]
                 }
             }
+        }
         case "remove":
             return {
                 ...state,
@@ -68,6 +68,7 @@ export const cartReducer = (state = cartInitState, action) => {
                 }).filter(item => item.quantity>0)
             }
 
-
+        default:
+            return state
     }
 }
